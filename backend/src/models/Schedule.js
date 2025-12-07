b27e08343fb74e6bd22498b8a3c4e4c1fefaa9db
Div801 <it21331190@my.sliit.lk>
2025-12-07 15:44:42 +0530

    create schedule model

--- backend/src/models/Schedule.js
+++ backend/src/models/Schedule.js
@@ -1,5 +1,21 @@
 import mongoose from "mongoose";
 
-const ScheduleSchema = new mongoose.Schema({});
+const { Schema } = mongoose;
+
+const ScheduleSchema = new Schema(
+  {
+    staffId: {
+      type: Schema.Types.ObjectId,
+      ref: "Staff",
+      required: true,
+    },
+    dayOfWeek: { type: Number, required: true },
+    startTime: String,
+    endTime: String,
+  },
+  { timestamps: true }
+);
+
+ScheduleSchema.index({ staffId: 1 });
 
 export default mongoose.model("Schedule", ScheduleSchema);
