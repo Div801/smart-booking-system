1eeeef111c06f6197931c50e368be449b2a9d93c
Div801 <it21331190@my.sliit.lk>
2025-12-07 12:53:17 +0530

    create service model

--- backend/src/models/User.js
+++ backend/src/models/User.js
@@ -1,5 +1,19 @@
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    passwordHash: { type: String, required: true },
+    role: {
+      type: String,
+      enum: ["admin", "staff", "customer"],
+      default: "customer",
+    },
+  },
+  { timestamps: true }
+);
+
+UserSchema.index({ email: 1 });
 
 export default mongoose.model("User", UserSchema);
