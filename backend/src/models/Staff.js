622702c4c0603ec98fccbf0cd174e119b838de29
Div801 <it21331190@my.sliit.lk>
2025-12-07 15:40:44 +0530

    create staff model

--- backend/src/models/Staff.js
+++ backend/src/models/Staff.js
@@ -1,5 +1,20 @@
 import mongoose from "mongoose";
 
-const StaffSchema = new mongoose.Schema({});
+const { Schema } = mongoose;
+
+const StaffSchema = new Schema(
+  {
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    specialties: [{ type: Schema.Types.ObjectId, ref: "Service" }],
+    availability: { type: Boolean, default: true },
+  },
+  { timestamps: true }
+);
+
+StaffSchema.index({ userId: 1 });
 
 export default mongoose.model("Staff", StaffSchema);
