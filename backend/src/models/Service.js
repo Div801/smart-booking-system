fdb1e32723adc07c0bc260b1aa22b477705318a2
Div801 <it21331190@my.sliit.lk>
2025-12-07 15:43:32 +0530

    create service model

--- backend/src/models/Service.js
+++ backend/src/models/Service.js
@@ -1,5 +1,15 @@
 import mongoose from "mongoose";
 
-const ServiceSchema = new mongoose.Schema({});
+const { Schema } = mongoose;
+
+const ServiceSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    durationMinutes: { type: Number, required: true },
+    price: { type: Number, required: true },
+    staffAllowed: [{ type: Schema.Types.ObjectId, ref: "Staff" }],
+  },
+  { timestamps: true }
+);
 
 export default mongoose.model("Service", ServiceSchema);
