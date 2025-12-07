7b2b0ceb6162fc5fd7429af0e0280e9abef6c2e3
Div801 <it21331190@my.sliit.lk>
2025-12-07 15:46:35 +0530

    create queue token model

--- backend/src/models/QueueToken.js
+++ backend/src/models/QueueToken.js
@@ -1,5 +1,22 @@
 import mongoose from "mongoose";
 
-const QueueTokenSchema = new mongoose.Schema({});
+const { Schema } = mongoose;
+
+const QueueTokenSchema = new Schema(
+  {
+    tokenNumber: Number,
+    staffId: { type: Schema.Types.ObjectId, ref: "Staff" },
+    customerId: { type: Schema.Types.ObjectId, ref: "User" },
+    status: {
+      type: String,
+      enum: ["waiting", "serving", "done"],
+      default: "waiting",
+    },
+    estimatedWaitMinutes: Number,
+  },
+  { timestamps: true }
+);
+
+QueueTokenSchema.index({ staffId: 1 });
 
 export default mongoose.model("QueueToken", QueueTokenSchema);
