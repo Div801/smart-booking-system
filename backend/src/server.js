628ece9cae6d5b16006464a829566bf5af7a99a2
Div801 <it21331190@my.sliit.lk>
2025-12-07 12:00:32 +0530

    add routes to server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import morgan from "morgan";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+import indexRoutes from "./routes/index.js";
 import { notFound, errorHandler } from "./middleware/errorHandler.js";
 
 dotenv.config();
@@ -14,6 +15,8 @@ app.use(express.json());
 app.use(morgan('dev'));
 
 connectDB();
+
+app.use("/api", indexRoutes);
 
 app.get('/', (req, res) => {
   res.json({ message: 'Smart Queue Backend Running 🚀' });
