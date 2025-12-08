88c8e2b447f9e545e39ea9470753d18f31a73f68
Div801 <it21331190@my.sliit.lk>
2025-12-08 13:01:50 +0530

    Add refresh token creation function

---
+++ backend/src/utils/generateRefreshToken.js
@@ -0,0 +1,9 @@
+import jwt from "jsonwebtoken";
+
+const generateRefreshToken = (user) => {
+  return jwt.sign({ id: user._id }, process.env.JWT_REFRESH_SECRET, {
+    expiresIn: "7d",
+  });
+};
+
+export { generateRefreshToken };
